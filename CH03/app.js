8b5db05d6e0498e1a4e0122f39a9d8d72c3c5ea3
kiminwoo <dlsdn166@gmail.com>
2021-06-22 14:43:50 +0900

    페이징 처리

--- CH03/app.js
+++ CH03/app.js
@@ -4,7 +4,9 @@ const content = document.createElement('div');
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-
+const store = {
+  currentPage :1 
+};
 
 function getData(url){
   ajax.open('GET', url, false);
@@ -18,10 +20,10 @@ function newsFeed(){
 
   newsList.push('<ul>');
 
-  for(let i = 0; i < 10; i++) {
+  for(let i = ( store.currentPage -1 ) * 10 ; i < store.currentPage * 10; i++) {
     newsList.push(`
       <li>
-        <a href="#${newsFeed[i].id}">
+        <a href="#/show/${newsFeed[i].id}">
           ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
       </li>
@@ -29,12 +31,17 @@ function newsFeed(){
   }
 
   newsList.push('</ul>');
-
+  newsList.push(`
+    <div>
+      <a href = "#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+      <a href = "#/page/${store.currentPage < 3 ? store.currentPage + 1 : 3}">다음 페이지</a>
+    </div>
+  `);
   container.innerHTML = newsList.join('');
 }
 
 function newsDetail() { // hash event 
-  const id = location.hash.substr(1);
+  const id = location.hash.substr(7);
 
   const newsContent = getData(CONTENT_URL.replace('@id',id))
   
@@ -42,7 +49,7 @@ function newsDetail() { // hash event
     <h1>${newsContent.title}</h1>
   
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
@@ -51,6 +58,9 @@ function router(){
   const routePath = location.hash;
 
   if(routePath === ''){ // location.hash 에 #만 있을 경우 빈문자열로 
+    newsFeed();
+  } else if (routePath.indexOf('#/page/')>=0){
+    store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else {
     newsDetail();
--- CH03/dist/app.c328ef1a.js
+++ CH03/dist/app.c328ef1a.js
@@ -123,6 +123,9 @@ var content = document.createElement('div');
 var ajax = new XMLHttpRequest();
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+var store = {
+  currentPage: 1
+};
 
 function getData(url) {
   ajax.open('GET', url, false);
@@ -135,19 +138,20 @@ function newsFeed() {
   var newsList = [];
   newsList.push('<ul>');
 
-  for (var i = 0; i < 10; i++) {
-    newsList.push("\n      <li>\n        <a href=\"#".concat(newsFeed[i].id, "\">\n          ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n      </li>\n    "));
+  for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push("\n      <li>\n        <a href=\"#/show/".concat(newsFeed[i].id, "\">\n          ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n        </a>\n      </li>\n    "));
   }
 
   newsList.push('</ul>');
+  newsList.push("\n    <div>\n      <a href = \"#/page/".concat(store.currentPage > 1 ? store.currentPage - 1 : 1, "\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n      <a href = \"#/page/").concat(store.currentPage < 3 ? store.currentPage + 1 : 3, "\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n    </div>\n  "));
   container.innerHTML = newsList.join('');
 }
 
 function newsDetail() {
   // hash event 
-  var id = location.hash.substr(1);
+  var id = location.hash.substr(7);
   var newsContent = getData(CONTENT_URL.replace('@id', id));
-  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n  \n    <div>\n      <a href=\"#\">\uBAA9\uB85D\uC73C\uB85C</a>\n    </div>\n  ");
+  container.innerHTML = "\n    <h1>".concat(newsContent.title, "</h1>\n  \n    <div>\n      <a href=\"#/page/").concat(store.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n    </div>\n  ");
 }
 
 function router() {
@@ -155,6 +159,9 @@ function router() {
 
   if (routePath === '') {
     // location.hash 에 #만 있을 경우 빈문자열로 
+    newsFeed();
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else {
     newsDetail();
